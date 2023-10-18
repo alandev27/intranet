41257f35e31375e809a78e3d054d4decb327bcaa
mesasalata <mete@san.gs>
2023-10-18 20:41:44 +0200

    feat: fixed some issues with class assignment

--- backend/server/core/lib/class_assign.py
+++ backend/server/core/lib/class_assign.py
@@ -39,16 +39,35 @@ class Timetable:
     def add_student_class(self, id_num, class_name):
         """Adds a class to a student's timetable, where it will fit."""
 
-        not_broken = True
-        for l in self.periods:  # each period [st1-cl1, st2-None, ...]
+        cap_not_exceeded = -2
+        for li in range(len(self.periods)):  # each period [st1-cl1, st2-None, ...]
+            l = self.periods[li]
             for m in l:  # each student's class
-                if m == class_name and not l[id_num]:
+                if cap_not_exceeded == -2:
+                    cap_not_exceeded = -1
+                class_counts = {}
+                for m in l + [class_name]:
+                    if m not in class_counts:
+                        class_counts[m] = 0
+                    class_counts[m] += 1
+                add = True
+                if l[id_num]:
+                    add = False
+                if class_name in self.class_caps.keys():
+                    if class_counts[class_name] > self.class_caps[class_name]:
+                        add = False
+                if add:
+                    if cap_not_exceeded == -1:
+                        cap_not_exceeded = li
+                if m == class_name and not l[id_num] and add:
+                    if cap_not_exceeded == -1:
+                        cap_not_exceeded = li
                     l[id_num] = class_name
-                    not_broken = False
-                    break
-        if not_broken:
-            added = False
-            for l in self.periods:  # each period [st1-cl1, st2-None, ...]
+                    return
+        added = False
+        if cap_not_exceeded != -1:
+            for li in range(max(0, cap_not_exceeded), len(self.periods)):  # each period [st1-cl1, st2-None, ...]
+                l = self.periods[li]
                 if not l[id_num]:
                     overlap = False
                     for m in range(len(self.teacher_classes)):  # each teacher by ID
@@ -63,22 +82,12 @@ class Timetable:
                         if overlap:
                             break
                     if not overlap:
-                        class_counts = {}
-                        for m in l + [class_name]:
-                            if m not in class_counts:
-                                class_counts[m] = 0
-                            class_counts[m] += 1
-                        add = True
-                        if class_name in self.class_caps.keys():
-                            if class_counts[class_name] > self.class_caps[class_name]:
-                                add = False
-                        if add:
-                            l[id_num] = class_name
-                            added = True
-                            break
-            if not added:
-                print("\033[31mError in agenda -> Errno 1\n\tStudent with ID " + str(id_num) + " and name " + self.student_name_dict[id_num] + " missing class " + class_name + ".\n\tCurrent agenda of student: " + str(self.get_student_classes(id_num)) + ".\033[0m")
-                return
+                        l[id_num] = class_name
+                        added = True
+                        break
+        if not added or cap_not_exceeded == -1:
+            print("\033[31mError in agenda -> Errno 1\n\tStudent with ID " + str(id_num) + " and name " + self.student_name_dict[id_num] + " missing class " + class_name + ".\n\tCurrent agenda of student: " + str(self.get_student_classes(id_num)) + ".\033[0m")
+            return
 
     def modify_student_class(self, id_num: int, period_index: int, new_class_name=None):
         """Changes the class of a student during a period. Defaults to removing the class."""
@@ -94,10 +103,6 @@ class Timetable:
             p.append(None)
         for p in class_names:  # each class
             self.add_student_class(id_num, p)
-        classes_left = class_names
-        for p in self.get_student_classes(id_num):
-            if p:
-                classes_left.remove(p)
         return id_num
 
     def add_teacher(self, class_names: list, teacher_name: str):
@@ -138,7 +143,7 @@ if __name__ == "__main__":
     student_name_dict = ["Mike", "Tom", "Bob"]  # The names of the students, from their IDs
     teacher_name_dict = ["Mr. B", "Ms. W"]  # The names of the teachers, from their IDs
     class_caps = {"Mathematics": 3}
-    student_classes = [["English (SL)", "Mathematics", "PST"], ["English (HL)", "Mathematics", "PST"], ["English (HL)", "Mathematics", "SPST"]]
+    student_classes = [["Mathematics", "English (SL)", "PST"], ["Mathematics", "English (HL)", "PST"], ["Mathematics", "English (HL)", "SPST"]]
     timetable = Timetable(3, student_classes, [["English (SL)", "English (HL)"], ["Mathematics"]], student_name_dict, teacher_name_dict, class_caps)
     for i in range(len(teacher_name_dict)):
         print("(Teacher) Classes of " + timetable.teacher_name_dict[i] + ":", timetable.get_teacher_classes(i))
